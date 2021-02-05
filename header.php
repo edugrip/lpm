fc6590bbe4727d013a3549ff2f13846fe3efed89
Ramesh Kumar <pareekbhagu@yahoo.com>
2021-02-05 16:19:59 +0530

    index

---
+++ header.php
@@ -0,0 +1,282 @@
+<!DOCTYPE html>
+<html lang="zxx">
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <title>LPM</title>
+    <meta name="description" content="LPM" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <link rel="profile" href="https://gmpg.org/xfn/11">
+    <link rel="canonical" href="https://htmldemo.hasthemes.com/furns/" />
+
+   <meta property="og:locale" content="en_US" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="LPM" />
+    <meta property="og:url" content="PAGE_URL" />
+    <meta property="og:site_name" content="LPM" />
+    <meta property="og:image" content="#" />
+    <meta property="og:description" content="LPM" />
+    <meta name="robots" content="noindex, follow" />
+    <!-- Add site Favicon -->
+    <link rel="icon" href="assets/images/favicon/favicon.png" sizes="32x32" />
+    <link rel="icon" href="assets/images/favicon/favicon.png" sizes="192x192" />
+    <link rel="apple-touch-icon" href="assets/images/favicon/favicon.png" />
+    
+    <link rel="stylesheet" href="assets/css/vendor/vendor.min.css" />
+    <link rel="stylesheet" href="assets/css/plugins/plugins.min.css" />
+    <link rel="stylesheet" href="assets/css/style.min.css">
+
+</head>
+
+<body>
+    <!-- Header Area start  -->
+    <div class="header section">
+        <!-- Header Top Message Start -->
+        <div class="header-top bg-black-0 section-fluid bg-black">
+            <div class="container">
+                <div class="row row-cols-lg-2 align-items-center">
+                    <!-- Header Top Message Start -->
+                    <div class="col text-center text-lg-left">
+                        <div class="header-top-massege">
+                            <p>Default Welcome Message </p>
+                        </div>
+                    </div>
+                    <!-- Header Top Message End -->
+                    <!-- Header Top Language Currency -->
+                    <div class="col d-none d-lg-block">
+                        <div class="header-top-set-lan-curr d-flex justify-content-end">
+                            <!-- Single Wedge Start -->
+                            <div class="header-top-curr dropdown">
+                                <button class="dropdown-toggle" data-bs-toggle="dropdown"> Language : (ENG) <i
+                                        class="ion-ios-arrow-down"></i></button>
+                                <ul class="dropdown-menu dropdown-menu-right">
+                                    <li><a class="dropdown-item" href="#">English</a></li>
+                                    <li><a class="dropdown-item" href="#">Italiano</a></li>
+                                    <li><a class="dropdown-item" href="#">Français</a></li>
+                                    <li><a class="dropdown-item" href="#">Filipino</a></li>
+                                </ul>
+                            </div>
+                            <!-- Single Wedge End -->
+                            <!-- Single Wedge Start -->
+                            <div class="header-top-curr dropdown">
+                                <button class="dropdown-toggle pr-0 border-0" data-bs-toggle="dropdown">Currency : (USD)
+                                    <i class="ion-ios-arrow-down"></i></button>
+                                <ul class="dropdown-menu dropdown-menu-right">
+                                    <li><a class="dropdown-item" href="#">USD $</a></li>
+                                    <li><a class="dropdown-item" href="#">EUR €</a></li>
+                                    <li><a class="dropdown-item" href="#">POUND £</a></li>
+                                    <li><a class="dropdown-item" href="#">FRANC ₣</a></li>
+                                </ul>
+                            </div>
+                            <!-- Single Wedge End -->
+                        </div>
+                    </div>
+                    <!-- Header Top Language Currency -->
+                </div>
+            </div>
+        </div>
+        <!-- Header Top  End -->
+        <!-- Header Bottom  Start -->
+        <div class="header-bottom d-none d-lg-block">
+            <div class="container position-relative">
+                <div class="row align-self-center">
+                    <!-- Header Logo Start -->
+                    <div class="col-auto align-self-center">
+                        <div class="header-logo">
+                            <a href="index.php"><img src="assets/images/logo/logo.png" alt="Site Logo" /></a>
+                        </div>
+                    </div>
+                    <!-- Header Logo End -->
+
+                    <!-- Header Action Start -->
+                    <div class="col align-self-center">
+                        <div class="header-actions">
+                            <div class="header_account_list">
+                                <a href="javascript:void(0)" class="header-action-btn search-btn"><i
+                                        class="icon-magnifier"></i></a>
+                                <div class="dropdown_search">
+                                    <form class="action-form" action="#">
+                                        <input class="form-control" placeholder="Enter your search key" type="text">
+                                        <button class="submit" type="submit"><i class="icon-magnifier"></i></button>
+                                    </form>
+                                </div>
+                            </div>
+                            <!-- Single Wedge Start -->
+                            <div class="header-bottom-set dropdown">
+                                <button class="dropdown-toggle header-action-btn" data-bs-toggle="dropdown"><i
+                                        class="icon-user"></i></button>
+                                <ul class="dropdown-menu dropdown-menu-right">
+                                    <li><a class="dropdown-item" href="my-account.html">My account</a></li>
+                                    <li><a class="dropdown-item" href="checkout.html">Checkout</a></li>
+                                    <li><a class="dropdown-item" href="login.html">Sign in</a></li>
+                                </ul>
+                            </div>
+                            <!-- Single Wedge End -->
+                            <a href="#offcanvas-cart" class="header-action-btn header-action-btn-cart offcanvas-toggle pr-0">
+                                <i class="icon-handbag"></i>
+                                <span class="header-action-num">01</span>
+                                <!-- <span class="cart-amount">€30.00</span> -->
+                            </a>
+                            <a href="#offcanvas-mobile-menu" class="header-action-btn header-action-btn-menu offcanvas-toggle d-lg-none">
+                                <i class="icon-menu"></i>
+                            </a>
+                        </div>
+                    </div>
+                    <!-- Header Action End -->
+                </div>
+            </div>
+        </div>
+        <!-- Header Bottom  End -->
+        <!-- Header Bottom  Start -->
+        <div class="header-bottom d-lg-none sticky-nav bg-white">
+            <div class="container position-relative">
+                <div class="row align-self-center">
+                    <!-- Header Logo Start -->
+                    <div class="col-auto align-self-center">
+                        <div class="header-logo">
+                            <a href="index.php"><img src="assets/images/logo/logo.png" alt="Site Logo" /></a>
+                        </div>
+                    </div>
+                    <!-- Header Logo End -->
+
+                    <!-- Header Action Start -->
+                    <div class="col align-self-center">
+                        <div class="header-actions">
+                            <div class="header_account_list">
+                                <a href="javascript:void(0)" class="header-action-btn search-btn"><i
+                                        class="icon-magnifier"></i></a>
+                                <div class="dropdown_search">
+                                    <form class="action-form" action="#">
+                                        <input class="form-control" placeholder="Enter your search key" type="text">
+                                        <button class="submit" type="submit"><i class="icon-magnifier"></i></button>
+                                    </form>
+                                </div>
+                            </div>
+                            <!-- Single Wedge Start -->
+                            <div class="header-bottom-set dropdown">
+                                <button class="dropdown-toggle header-action-btn" data-bs-toggle="dropdown"><i
+                                        class="icon-user"></i></button>
+                                <ul class="dropdown-menu dropdown-menu-right">
+                                    <li><a class="dropdown-item" href="my-account.html">My account</a></li>
+                                    <li><a class="dropdown-item" href="checkout.html">Checkout</a></li>
+                                    <li><a class="dropdown-item" href="login.html">Sign in</a></li>
+                                </ul>
+                            </div>
+                            <!-- Single Wedge End -->
+                            <a href="#offcanvas-cart" class="header-action-btn header-action-btn-cart offcanvas-toggle pr-0">
+                                <i class="icon-handbag"></i>
+                                <span class="header-action-num">01</span>
+                                <!-- <span class="cart-amount">€30.00</span> -->
+                            </a>
+                            <a href="#offcanvas-mobile-menu" class="header-action-btn header-action-btn-menu offcanvas-toggle d-lg-none">
+                                <i class="icon-menu"></i>
+                            </a>
+                        </div>
+                    </div>
+                    <!-- Header Action End -->
+                </div>
+            </div>
+        </div>
+        <!-- Header Bottom  End -->
+        <!-- Main Menu Start -->
+        <div class="bg-black d-none d-lg-block sticky-nav">
+            <div class="container position-relative">
+                <div class="row">
+                    <div class="col-md-12 align-self-center">
+                        <div class="main-menu">
+                            <ul>
+                                <li><a href="#">Home</a>
+                                   
+                                </li>
+                                <li><a href="#">About us</a></li>
+                                <li><a href="single-product.php">Shop</a></li>
+                                <li><a href="#">Pages</a></li>
+                                <li><a href="#">Blog</a></li>
+                                <li><a href="#">Contact us</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Main Menu End -->
+    </div>
+    <!-- Header Area End  -->
+
+    <!-- OffCanvas Cart Start -->
+    
+    <!-- OffCanvas Cart End -->
+
+    <!-- OffCanvas Menu Start -->
+    <div id="offcanvas-mobile-menu" class="offcanvas offcanvas-mobile-menu">
+        <button class="offcanvas-close"></button>
+        <div class="inner customScroll">
+
+            <div class="offcanvas-menu mb-4">
+                <ul>
+                    <li><a href="#">Home</a>
+                                   
+                                </li>
+                                <li><a href="#">About us</a></li>
+                                <li><a href="#">Shop</a></li>
+                                <li><a href="#">Pages</a></li>
+                                <li><a href="#">Blog</a></li>
+                                <li><a href="#">Contact us</a></li>
+                    
+                </ul>
+            </div>
+            <!-- OffCanvas Menu End -->
+
+            <!-- Language Currency start -->
+            <div class="offcanvas-userpanel mt-8">
+                <ul>
+                    <!-- Language Start -->
+                    <li class="offcanvas-userpanel__role">
+                        <a href="#">English <i class="ion-ios-arrow-down"></i></a>
+                        <ul class="user-sub-menu">
+                            <li><a class="current" href="#">English</a></li>
+                            <li><a href="#"> Italiano</a></li>
+                            <li><a href="#"> Français</a></li>
+                            <li><a href="#"> Filipino</a></li>
+                        </ul>
+                    </li>
+                    <!-- Language End -->
+                    <!-- Currency Start -->
+                    <li class="offcanvas-userpanel__role">
+                        <a href="#">USD $ <i class="ion-ios-arrow-down"></i></a>
+                        <ul class="user-sub-menu">
+                            <li><a class="current" href="#">USD $</a></li>
+                            <li><a href="#">EUR €</a></li>
+                            <li><a href="#">POUND £</a></li>
+                            <li><a href="#">FRANC ₣</a></li>
+                        </ul>
+                    </li>
+                    <!-- Currency End -->
+                </ul>
+            </div>
+            <!-- Language Currency End -->
+            <div class="offcanvas-social mt-auto">
+                <ul>
+                    <li>
+                        <a href="#"><i class="ion-social-facebook"></i></a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="ion-social-twitter"></i></a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="ion-social-google"></i></a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="ion-social-youtube"></i></a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="ion-social-instagram"></i></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <!-- OffCanvas Menu End -->
+      <div class="offcanvas-overlay"></div>
+
